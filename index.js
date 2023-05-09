e998d701ac7c12d019a15dc6807ca64f61c550ba
guqiao2011 <1015877902@qq.com>
2023-05-09 11:14:05 +0000

    Update index.js

--- index.js
+++ index.js
@@ -4,4 +4,7 @@ app.all('/', (req, res) => {
     console.log("Just got a request!")
     res.send('Yo!')
 })
+app.get('/test',(req, res) =>{
+    res.json({"key":"value"})
+})
 app.listen(process.env.PORT || 3000)
